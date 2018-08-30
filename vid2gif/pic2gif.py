84f3e00a8005e8d26647865b07c0335197eb16a6
David Chu <david.chu@dcard.cc>
2018-08-30 18:34:18 +0800

    Fix

--- vid2gif/pic2gif.py
+++ vid2gif/pic2gif.py
@@ -13,6 +13,7 @@ def to_gif(filename, array, fps=12, scale=1.0):
         array = array[..., np.newaxis] * np.ones(3)
 
     # make the moviepy clip
-    clip = ImageSequenceClip(list(array), fps=fps).resize(scale)
+    clip = ImageSequenceClip(list(array), fps=fps)
+    clip.resize(scale)
     clip.write_gif(filename, fps=fps)
     return clip
--- vid2gif/vid2pic.py
+++ vid2gif/vid2pic.py
@@ -1,11 +1,24 @@
-import numpy as np
+from pathlib import Path
+
 import skvideo.io
 import skimage.transform
 
 
-def to_pic(fname, sample_rate=12):
-    vid = skvideo.io.vreader(fname)
-    frames = [skimage.transform.rescale(frame, 1, mode='constant', preserve_range=True)
-              for i, frame in enumerate(vid) if i % sample_rate == 0]
-    frames = np.array(frames).astype(np.uint8)
-    return frames
+def to_pic(fname, to_folder, sample_rate=12):
+    folder = Path(to_folder)
+    folder.mkdir(parents=True, exist_ok=True)
+
+    fname = Path(fname)
+    assert fname.exists()
+
+    vid = skvideo.io.vreader(str(fname))
+    outfile_names = []
+    for i, frame in enumerate(vid):
+        if i % sample_rate == 0:
+            frame = skimage.transform.rescale(frame, 1, mode='constant', preserve_range=True)
+            outfile_name = str(folder / '%s-%s.png' % (fname.stem, i))
+            skimage.io.imsave(outfile_name, frame)
+            outfile_names.append(outfile_name)
+        else:
+            continue
+    return outfile_names
